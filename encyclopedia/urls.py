8a9acb636b998fe9cdcc56cb74d25374536e58a4
ke3peR007 <miteshvp13@gmail.com>
2020-10-18 03:24:42 +0530

    added index page links

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+# app_name = "wiki"
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:name>", views.get_page, name="get_page")
